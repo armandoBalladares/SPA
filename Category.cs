fac80f7b2fb0912cb842bbc7de4dc9e096543db4
Juan A. Sandino B. <juan.armando.sandino.ba@outlook.com>
2024-03-21 14:49:02 -0600

    initial all component

---
+++ Category.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPA
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+
+        public virtual ObservableCollectionListSource<Product> Products { get; } = new();
+    }
+}
