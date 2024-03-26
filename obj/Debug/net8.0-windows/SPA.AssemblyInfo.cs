33e582ce4132c9e161f6224bd5ffa833b6d7a2a5
Juan A. Sandino B. <juan.armando.sandino.ba@outlook.com>
2024-03-26 17:43:27 -0600

    refactor chore;  remove unnecessary entities

--- obj/Debug/net8.0-windows/SPA.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SPA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SPA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37142e6624d152c38a389b5ff05de404a8f3c2de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b2d44407b2c0c16cda52efbfee638175557d96")]
 [assembly: System.Reflection.AssemblyProductAttribute("SPA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SPA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
